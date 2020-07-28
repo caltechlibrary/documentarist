70dc70568ae031a3c2ec5f3d5d6e854441210929
Michael Hucka <mhucka@caltech.edu>
2020-07-27 21:15:02 -0700

    Start a module for TextForm

---
+++ documentarist/textform/textform/__init__.py
@@ -0,0 +1,20 @@
+'''
+textform: report whether image contains printed or handwritten text
+
+This module implements a classifier that takes an image (usually a region
+extracted from a larger image) believed to contain text, and evaluates whether
+the text appears to be printed or handwritten in form.
+
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
