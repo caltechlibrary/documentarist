2e168b36323706506b937457aeb25e7e67ebb628
Michael Hucka <mhucka@caltech.edu>
2020-08-09 20:59:12 -0700

    Add writable() from my other programs

--- common/file_utils.py
+++ common/file_utils.py
@@ -16,6 +16,7 @@ file "LICENSE" for more information.
 
 import os
 from   os import path
+import tempfile
 
 
 # Main functions.
@@ -24,3 +25,28 @@ from   os import path
 def readable(dest):
     '''Returns True if the given 'dest' is accessible and readable.'''
     return os.access(dest, os.F_OK | os.R_OK)
+
+
+def writable(dest):
+    '''Returns True if the destination is writable.'''
+
+    # Helper function to test if a directory is writable.
+    def dir_writable(dir):
+        # This is based on the following Stack Overflow answer by user "zak":
+        # https://stackoverflow.com/a/25868839/743730
+        try:
+            testfile = tempfile.TemporaryFile(dir = dir)
+            testfile.close()
+        except (OSError, IOError) as e:
+            return False
+        return True
+
+    if path.exists(dest) and not path.isdir(dest):
+        # Path is an existing file.
+        return os.access(dest, os.F_OK | os.W_OK)
+    elif path.isdir(dest):
+        # Path itself is an existing directory.  Is it writable?
+        return dir_writable(dest)
+    else:
+        # Path is a file but doesn't exist yet. Can we write to the parent dir?
+        return dir_writable(path.dirname(dest))
