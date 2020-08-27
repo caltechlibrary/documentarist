3733534d9ff8f180a4da741731073dcbf5fadce6
Michael Hucka <mhucka@caltech.edu>
2020-08-27 12:41:15 -0700

    Update for changes in exit_codes.py

--- tests/test_exit_codes.py
+++ tests/test_exit_codes.py
@@ -4,5 +4,5 @@ from common.exit_codes import *
 
 def test_exit_code():
     assert ExitCode.bad_arg.value[0] == 2
-    assert ExitCode.exception.value[0] == 4
+    assert ExitCode.exception.value[0] == 5
     assert ExitCode.success.value[1] == 'success -- program completed normally'
