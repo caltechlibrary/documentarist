b0afc131635cc6684b7f7b6e35c75c4b0bc9b97a
Michael Hucka <mhucka@caltech.edu>
2020-10-21 17:26:36 -0700

    Use new enhanced enum-based approach

--- common/exit_codes.py
+++ common/exit_codes.py
@@ -14,13 +14,30 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from aenum import Enum, unique
+from aenum import Enum, MultiValue
 
-@unique
+# I adapted the clever approach posted by the author of the Python aenum
+# package, Ethan Furman, to Stack Overflow on 2016-03-13 at
+# https://stackoverflow.com/a/35964875/743730
+# The most important bit is realizing you can define __int__().
+
 class ExitCode(Enum):
-    success        = 0, 'success -- program completed normally'
-    no_net         = 1, 'no network detected'
-    bad_arg        = 2, 'encountered a bad or missing value for an option'
-    file_error     = 3, 'encountered a problem with a file'
-    user_interrupt = 4, 'the user interrupted execution'
-    exception      = 5, 'an exception or fatal error occurred'
+    '''Class of exit codes that this program may return.
+
+    The numeric value of a given code can be obtained by using int().  For
+    example, int(ExitCode.success) will produce 0.
+    '''
+
+    _init_ = 'value meaning'
+    _settings_ = MultiValue
+
+    success        = 0, "success -- program completed normally"
+    user_interrupt = 1, "the user interrupted the program's execution"
+    bad_arg        = 2, "encountered a bad or missing value for an option"
+    no_network     = 3, "no network detected -- cannot proceed"
+    file_error     = 4, "file error -- encountered a problem with a file or directory"
+    server_error   = 5, "server error -- encountered a problem with the server"
+    exception      = 6, "an exception or fatal error occurred"
+
+    def __int__(self):
+        return self.value
