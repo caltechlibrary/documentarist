a053b46e57042b41ba42697e78f3aaa788e88f5c
mhucka <mhucka@caltech.edu>
2021-07-07 11:15:01 -0700

    Use a base class for command parsers

---
+++ documentarist/command.py
@@ -0,0 +1,129 @@
+'''
+command.py: base class for command-line command interfaces
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020-2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from   argparse import ArgumentParser, RawDescriptionHelpFormatter
+from   inspect import cleandoc
+import re
+from   shutil import get_terminal_size
+from   sidetrack import set_debug, log, logr
+from   textwrap import wrap, fill, dedent
+
+from   common.exit_codes import ExitCode
+
+
+# Exported classes.
+# .............................................................................
+
+class Command():
+    '''Base class for Documentarist command-line command parsers.'''
+
+    def __init__(self, name, arg_list):
+        self._name = name
+
+        usage = f'%(prog)s {name}{" " if name else ""}[subcommand]'
+        parser = ArgumentParser(description = docstring_summary(self, name),
+                                formatter_class = RawDescriptionHelpFormatter,
+                                add_help = False, usage = usage)
+
+        parser.add_argument('subcommand', nargs = '*',
+                            help = 'Available subcommands: ' + command_list(self))
+
+        # Skip the command name in the argument list.
+        args = parser.parse_args(arg_list)
+        if args.subcommand:
+            command_name = args.subcommand[0]
+            if hasattr(self, command_name):
+                log(f'dispatching to {command_name}')
+                getattr(self, command_name)(args.subcommand[1:])
+            else:
+                alert_fatal(f'Unrecognized command: "{command_name}"')
+                parser.print_help()
+                raise CannotProceed(ExitCode.bad_arg)
+        else:
+            parser.print_help()
+
+
+    def help(self, args):
+        '''Print detailed help information, and exit.'''
+        parser = ArgumentParser(description = 'Print help for commands',
+                                usage = '%(prog)s {self._name} help [name]')
+
+        parser.add_argument('name', nargs = '?', action = 'store')
+        subargs = parser.parse_args(args)
+        if subargs.name is None:
+            print(class_help(self, self._name))
+        elif subargs.name in dir(self):
+            docstring = getattr(self, subargs.name).__doc__
+            print(safely_wrapped(cleandoc(docstring)))
+        else:
+            alert(f'Unrecognized command: "{subargs.name}"')
+            print(class_help(self, self._name))
+
+
+# Utility functions.
+# .............................................................................
+
+def command_list(cls):
+    methods = [name for name in dir(cls) if not name.startswith('_')]
+    return ', '.join(f'"{name}"' for name in methods)
+
+
+def docstring_summary(cls, cmd_name = ''):
+    text = safely_wrapped(cleandoc(cls.__class__.__doc__))
+    text += '\n\nThe following commands are available:\n\n'
+
+    # Find the longest name, to help compute the indentation of the 2nd column.
+    longest = max(len(name) for name in dir(cls) if not name.startswith('_'))
+    indent = longest + 2
+    for name in [name for name in dir(cls) if not name.startswith('_')]:
+        docstring = getattr(cls, name).__doc__
+        if not docstring:
+            continue
+        first_line = docstring.split('\n')[0].rstrip('.')
+        text += f'  {name}{" "*(indent - len(name))}{first_line}\n'
+    return text
+
+
+def class_help(cls, cmd_name = ""):
+    text = docstring_summary(cls)
+    for name in [name for name in dir(cls) if not name.startswith('_')]:
+        if cmd_name:
+            text += '\n' + cmd_name + ' ' + name + '\n'
+            text += '~'*(len(cmd_name) + len(name) + 1) + '\n\n'
+        else:
+            text += '\n' + name + '\n'
+            text += '~'*(len(name) + 1) + '\n\n'
+        method_docstring = cleandoc(getattr(cls, name).__doc__)
+        text += safely_wrapped(method_docstring) + '\n'
+    return text
+
+
+def wrapped(string, width = get_terminal_size().columns - 4):
+    return '\n'.join(wrap(string, width))
+
+
+def safely_wrapped(content):
+    '''Wrap the given content, being careful not to touch indented lines.'''
+    # Replace leading blanks w/ special char so we can find them after wrapping.
+    indents_saved = re.sub(r'^  +', '⁌', content, flags = re.MULTILINE)
+    # Split the text at double newlines (=> paragraphs).
+    paras = indents_saved.replace('\n\n', '⁍').split('⁍')
+    # Wrap those paras that don't have leading blanks.
+    paras = map(lambda s: s if s.startswith('⁌') else wrapped(s), paras)
+    # Put back the leading spaces.
+    paras = map(lambda s: re.sub('⁌', '  ', s), paras)
+    # Put back inter-paragraph line breaks, and we're done.
+    return '\n\n'.join(paras)
---
+++ documentarist/config.py
@@ -0,0 +1,144 @@
+'''
+config.py: Handle Documentarist configuration commands
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020-2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from   argparse import ArgumentParser, RawDescriptionHelpFormatter
+from   bun import UI, inform, warn, alert, alert_fatal
+
+from   documentarist.command import Command
+
+
+# Constants.
+# .............................................................................
+
+DEFAULT_CONFIG_FILE = 'documentarist.ini'
+'''
+The default name of a configuration file that Documentarist will look for in
+its installation directory if not explicitly given a configuration file to use.
+The file must be in the format understood by Python 3's ConfigParser,
+(c.f. https://docs.python.org/3/library/configparser.html).
+'''
+
+
+# Exported classes.
+# .............................................................................
+
+class Config(Command):
+    '''Set or show Documentarist's configuration.
+
+    Documentarist has a number of configuration parameters that control its
+    behavior.  The values of the parameters can be set in a configuration file.
+    Documentarist looks for a configuration file in the following locations
+    each time it runs, in this order:
+
+      1. the file given as the value of the --configfile option
+      2. a file named "documentarist.ini" the current directory
+      3. a file named "documentarist.ini" either in the directory
+         ~/Library/Application Support/Documentarist/ (macOS) or
+         ~/.config/documentarist/ (Linux)
+      4. a file named "documentarist.ini" in the directory where Documentarist
+         is installed on the system
+
+    The "config" commands can be used to display current configuration values
+    as well as set configuration values.
+    '''
+
+    def __init__(self, arg_list):
+        super().__init__("config", arg_list)
+
+
+    def show(self, args):
+        '''Print the current configuration and exit.'''
+        print('invoked config show')
+
+
+    def basename(self, args):
+        '''Set the basename for files downloaded over the network.
+
+        When the inputs are URLs, Documentarist must download a copy of the
+        image located at the network address (because it is not possible to
+        write the results in the network locations represented by the URLs).
+        The image at networked locations will be converted to ordinary PNG
+        format for maximum compatibility with the different OCR services and
+        written to files whose root names have the form "document-N.png",
+        where "N" is an integer.  The URL corresponding to each document will
+        be written in a file named "document-N.url" so that it is possible to
+        connect each "document-N.png" to the URL it came from.
+
+        The root name can be changed to something other than "document" using
+        the "config basename" command.  For example,
+
+            dm config basename someothername
+
+        will change the naming pattern to "someothername-N".
+        '''
+
+        print(f'in basename got {args}')
+        parser = ArgumentParser(description = 'Set the basename for downloaded files',
+                                usage = '%(prog)s config basename [-h] name')
+
+        parser.add_argument('name', action = 'store')
+        subargs = parser.parse_args(args)
+        print(f'result = {subargs}')
+
+
+    def outputdir(self, args):
+        '''Set the output directory where files will be written.
+
+        When processing images, Documentarist writes the results to new files
+        that it creates either in the same directories as the original files,
+        or alternatively, to a directory set by the command "config
+        outputdir".  When an output directory has not been set and the input
+        images are given as URLs, then the files are written to the current
+        working directory instead.  The "config outputdir" command takes a
+        directory path as its argument.  For example,
+
+            dm config outputdir /tmp
+
+        will change the output directory to /tmp.
+        '''
+        print('invoked config outputdir')
+
+
+    def auth(self, args):
+        '''Configure credentials for cloud services.
+
+        Before a given service can be used, Documentarist needs to be
+        supplied with user credentials for accessing that service.  The
+        credentials must be stored in a JSON file with a certain format; see
+        the Documentarist documentation for details about the formats for
+        each service.  To add a new credentials file, use the "config auth"
+        option in combination with the name of a service and a single file
+        path on the command line.  The name supplied right after the "config
+        auth" option must be the name of a recognized service, and the file
+        argument must be a JSON file containing the credentials data in the
+        required format for that service.  Here is an example of adding
+        credentials for Google (assuming you created the JSON file as
+        described in the docs):
+
+          dm config auth google mygooglecreds.json
+
+        Run dm with the "config auth" command separately to install
+        credentials for each service.  Documentarist will copy the
+        credentials to its configuration file and exit without doing anything
+        else.
+
+        '''
+        print('invoked config auth')
+
+
+    def color(self, args):
+        '''Turn on or off coloring of terminal output.'''
+        print('invoked config color')
