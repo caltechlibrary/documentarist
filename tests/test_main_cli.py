5c37ff1125e1688636847613fea1822e043d3c0a
mhucka <mhucka@caltech.edu>
2021-07-26 15:22:29 -0700

    Add unit tests for main CLI interface

---
+++ tests/test_main_cli.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+sys.path.append('../common')
+from common.exceptions import *
+
+def test_main_help(capsys):
+    result = subprocess.run(['bin/dm'], capture_output = True)
+    assert result.stdout.startswith(b'usage: dm [-h]')
+
+    result = subprocess.run(['bin/dm', '-h'], capture_output = True)
+    assert result.stdout.startswith(b'usage: dm [-h]')
+
+    result = subprocess.run(['bin/dm', 'help'], capture_output = True)
+    assert result.stdout.startswith(b'"dm" is the command-line interface for')
+
+    result = subprocess.run(['bin/dm', 'help', 'version'], capture_output = True)
+    assert result.stdout.startswith(b'Print Documentarist version information, and exit.')
+
+
+def test_main_version(capsys):
+    result = subprocess.run(['bin/dm', 'version'], capture_output = True)
+    assert result.stdout.startswith(b'Documentarist version')
