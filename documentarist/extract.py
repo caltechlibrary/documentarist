e30bee783a8cb9e6cfb5525c6ed3a76697de47b6
mhucka <mhucka@caltech.edu>
2022-05-05 12:37:30 -0700

    Add missing imports

--- documentarist/extract.py
+++ documentarist/extract.py
@@ -2,5 +2,10 @@
 extract.py: extract text from an image
 '''
 
+from   documentarist.command import Command, docstring_summary
+from   documentarist.exceptions import FileError
+from   documentarist.log import log
+from   documentarist.ui import UI, inform, warn, alert
+
 class ExtractCommand(Command):
     '''Extract text found in files or URLs.'''
